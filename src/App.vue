e8880334dcbf15c9dc63fb36f35d0b087e945aec
Shivam Goswami <shiv15101990@gmail.com>
2019-11-17 18:50:26 +0530

    Responsive cards for rooms

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
     <div id="app">
         <v-app :style="{background: 'black'}">
-            <v-app-bar app clipped-left color="black" tile height="70px" flat elevation-1 absolute>
-                <v-spacer></v-spacer>
+            <v-app-bar app color="black" tile height="70px" flat elevation-1 absolute>
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
                 <v-avatar width="200px" tile>
                     <v-img :src="logo" contain></v-img>
                 </v-avatar>
-                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
                 <v-spacer></v-spacer>
 
-                <v-btn text height="100%" class="font-weight-bold">The Villas</v-btn>
-                <v-btn text height="100%" class="font-weight-bold">Facilities</v-btn>
-                <v-btn text height="100%" class="font-weight-bold">Tours</v-btn>
-                <v-btn text height="100%" class="font-weight-bold">Restaurant</v-btn>
-                <v-btn text height="100%" class="font-weight-bold">Gallery</v-btn>
-                <v-btn text height="100%" class="font-weight-bold">Contact Us</v-btn>
+                <div class="hidden-md-and-down">
+                    <v-btn text height="100%" class="font-weight-bold" v-for="(item, index) in navItems" :key="index">
+                        {{item.title}}
+                    </v-btn>
+                </div>
 
                 <v-spacer></v-spacer>
 
                 <v-btn color="primary" height="80%" width="170px" class="black--text" tile>Book Now</v-btn>
-                <v-spacer></v-spacer>
             </v-app-bar>
+
+            <v-navigation-drawer v-model="drawer" temporary absolute app clipped color="black" :width=" $vuetify.breakpoint.mdAndDown ? '100%': '30%'">
+                <v-icon size="35" style="position: absolute; right: 10px; top: 10px" @click.stop="drawer = !drawer">
+                    mdi-close
+                </v-icon>
+                <v-layout justify-center align-center fill-height column>
+                    <v-list>
+                        <v-list-item class="text-center headline hidden-md-and-up" v-for="(item, index) in navItems" :key="index">
+                            {{item.title}}
+                        </v-list-item>
+                        <v-list-item class="text-center headline">Gallery</v-list-item>
+                        <v-list-item class="text-center headline">Exclusive Bookings</v-list-item>
+                    </v-list>
+                </v-layout>
+
+
+            </v-navigation-drawer>
 
             <v-content>
                 <router-view/>
@@ -33,6 +47,14 @@
     #app {
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+
+        nav {
+            z-index: 9999;
+        }
+
+        .pl-large {
+            padding-left: 200px;
+        }
     }
 </style>
 
@@ -41,9 +63,15 @@
     export default {
         data() {
             return {
-                drawer: null,
+                drawer: false,
                 logo: logo,
-                videoHeight: null
+                videoHeight: null,
+                navItems: [
+                    { title: "The Villas"},
+                    { title: "Facilities"},
+                    { title: "The Villas"},
+                    { title: "Contact Us"},
+                ]
             }
         }
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
             :mp4Source="require('../assets/background.mp4')"
             :mobileBreakpoint="992"
             :desktopHeight="videoHeight"
-            :mobileHeight="450"
+            :mobileHeight="videoHeight"
             :playsinline="true"
             :loop="true"
             :autoplay="true"
@@ -15,26 +15,57 @@
             :muted="true"
     >
       <div slot="overlay-content" class="overlay-content">
-        <v-container fluid :style="{ height: videoHeight + 'px' }">
+        <v-container :style="{ height: videoHeight + 'px' }">
           <v-layout justify-center align-start fill-height column wrap id="video-section">
             <div class="title text--secondary">Sri Lanka</div>
-            <div class="display-4 white--text">Wadduwa Beach Villas</div>
+            <h1 class="display-4 white--text">Wadduwa Beach Villas</h1>
             <div class="display-1 white--text text--secondary">A local icon of private luxury</div>
+            <a class="title" :style="{ color: $vuetify.theme.currentTheme.primary }">
+              Find out more about our packages
+              <v-icon color="primary" size="30">mdi-chevron-right</v-icon>
+            </a>
+
           </v-layout>
         </v-container>
       </div>
     </vue-video-section>
     <!-- Video section -->
 
-    <h2 class="white--text">For our guests</h2>
+    <!-- Rooms section -->
+    <v-container class="pt-12">
+      <h2 class="display-2">The Villas</h2>
+      <p class="title text--secondary text-justify">
+        We provide a range of accommodation options from Superior cottages to Deluxe double occupancy rooms.
+        The main difference in each is the size of the rooms and living areas and the size of the beds.
+        The bungalows are situated on an one and a half acre (6070 square meter) landscaped, grassed land with
+        coconut and other native trees.
+      </p>
+      <v-row no-gutters>
+        <v-col cols="12" v-for="(room, index) in rooms" :key="index" md="4">
+          <v-card tile color="white" class="black--text ma-4" height="470px">
+            <v-img height="250" :src="room.titleImg" class="white--text align-end" gradient="to bottom, transparent, rgba(0,0,0,0.8)">
+              <v-card-title class="font-weight-bold">{{room.title}}</v-card-title>
+            </v-img>
+
+            <v-chip label color="primary" class="black--text font-weight-bold" style="position: absolute; top: 0; right: 0">
+              USD 140
+            </v-chip>
+
+            <v-btn tile block color="primary" class="elevation-0 black--text font-weight-bold" height="45px">Book now</v-btn>
+            <v-card-text class="black--text text-justify">
+              {{room.description}}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- Rooms section -->
   </div>
 </template>
 
 <style lang="scss">
   #video-section {
-    padding-left: 200px;
-
-    div.title, div.display-4 {
+    div.title, h1 {
       padding-bottom: 20px;
     }
   }
@@ -47,6 +78,9 @@
 <script>
 // @ is an alias to /src
 import VueVideoSection from 'vue-video-section';
+import room1 from "@/assets/room-1.jpg";
+import room2 from "@/assets/room-2.jpg";
+import room3 from "@/assets/room-3.jpg";
 
 export default {
   components: {
@@ -54,11 +88,20 @@ export default {
   },
   data() {
     return {
-      videoHeight: null
+      videoHeight: null,
+      room1: room1,
+      room2: room2,
+      room3: room3,
+      rooms: [
+        {title: "Superior Two Bedroom Cottage", titleImg: room1, description: "The Superior Two Bedroom Cottage has a pool view and is located 30 meters from the sea shore. It has two separate bedrooms with attached bathroos, one with a sky view bathroom. This cottage includes a sitting and dining room with full kitchen, as well as an outside veranda and seating. Sleeps four adults."},
+        {title: "Deluxe Two Bedroom Bungalow", titleImg: room2, description: "The Deluxe Two Bedroom Cottage has both a beach and pool view. The two rooms have two separate attached bathrooms and a kitchen. It has an enclosed veranda with 180 degree view. Sleeps four adults."},
+        {title: "Just Two- Secluded Cottage", titleImg: room3, description: "The Just Two- Secluded Cottage has both a pool and beach view. The single bedroom cottage has an attached bathroom with kitchen and veranda which offers a 180 degree view of the greenery and lawn. It is secluded and offers privacy as it is located in a secluded area of the property. Sleeps two adults."},
+      ]
     }
   },
   mounted() {
     this.videoHeight = this.$vuetify.breakpoint.height - 75;
+    console.log(this.$vuetify.breakpoint);
   }
 }
 </script>
