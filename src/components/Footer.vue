69c302c3c35ee0d3a7985dae18a09984121ed412
Shivam Goswami <shiv15101990@gmail.com>
2020-02-22 01:57:17 +1100

    Added px-to-rem. Changed footer font

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,57 +3,64 @@
         <v-footer color="black">
             <v-container>
                 <v-row no-gutters>
-                    <v-col cols="12" md="4" sm="12" class="text-center">
+                    <v-col cols="12" md="3" sm="12" class="text-center">
                         <v-avatar width="200px" tile>
                             <v-img :src="logo" contain></v-img>
                         </v-avatar>
                         <ul id="sitemap-links">
                             <li v-for="(item, index) in $attrs.items" :key="index"
-                                @click="$router.push({ name: item.linkTo})">
+                                @click="$router.push({ name: item.linkTo})" class="caption">
                                 {{item.title}}
                             </li>
                         </ul>
                     </v-col>
 
-                    <v-col cols="12" md="4" sm="12" class="text-center">
-                        <h3 class="pb-6">Contact Us</h3>
+                    <v-col cols="12" md="6" sm="12">
+                        <h3 class="pb-6 body-1 font-weight-bold text-center">Contact Us</h3>
 
                         <div id="contactus-list">
                             <div class="contactus-item">
                                 <v-icon color="primary">mdi-map-marker</v-icon>
-                                <p>Wadduwa Beach Villa 36/17, Sanath Perera Mawatha, South Thalpitiya, Wadduwa, Sri Lanka</p>
+                                <p class="caption">
+                                    Wadduwa Beach Villa 36/17, Sanath Perera Mawatha,
+                                    South Thalpitiya, Wadduwa, Sri Lanka
+                                </p>
                             </div>
 
                             <div class="contactus-item">
                                 <v-icon color="primary">mdi-phone</v-icon>
-                                <p>+94 712 532 311 / +94 713 770 692 / +94 382 295 758 / +94 710 150 415</p>
+                                <p class="caption">
+                                    +94 712 532 311 / +94 713 770 692 / +94 382 295 758 / +94 710 150 415
+                                </p>
                             </div>
 
                             <div class="contactus-item">
                                 <v-icon color="primary">mdi-email</v-icon>
-                                <p>wadduwabeachvilla@gmail.com,  wadduwabeachvilla@outlook.com</p>
+                                <p class="caption">
+                                    wadduwabeachvilla@gmail.com,  wadduwabeachvilla@outlook.com
+                                </p>
                             </div>
                         </div>
                     </v-col>
 
-                    <v-col cols="12" md="4" sm="12" class="text-center">
-                        <h3 class="pb-6">Social</h3>
+                    <v-col cols="12" md="3" sm="12" class="text-center">
+                        <h3 class="pb-6  body-1 font-weight-bold">Social</h3>
 
                         <v-layout justify-center>
                             <a target="_blank" href="https://www.facebook.com/wadduwabeachvillasPvtLtd/">
-                                <v-img :src="fb" max-width="40" max-height="40" contain class="ml-2"></v-img>
+                                <v-img :src="fb" max-width="30" max-height="30" contain class="ml-2"></v-img>
                             </a>
                             <a target="_blank" href="https://www.tripadvisor.com/RentalPropertyManager-a_manager.236794">
-                                <v-img :src="ta" max-width="40" max-height="40" contain class="ml-2"></v-img>
+                                <v-img :src="ta" max-width="30" max-height="30" contain class="ml-2"></v-img>
                             </a>
                             <a target="_blank" href="https://www.airbnb.com/users/show/19675962">
-                                <v-img :src="ab" max-width="40" max-height="40" contain class="ml-2"></v-img>
+                                <v-img :src="ab" max-width="30" max-height="30" contain class="ml-2"></v-img>
                             </a>
                             <a target="_blank" href="https://www.instagram.com/wadduwa.beach.villas/">
-                                <v-img :src="ig" max-width="40" max-height="40" contain class="ml-2"></v-img>
+                                <v-img :src="ig" max-width="30" max-height="30" contain class="ml-2"></v-img>
                             </a>
                             <a target="_blank" href="https://www.expedia.com/Wadduwa-Hotels-Wadduwa-Beach-Villas-Pvt-Ltd.h42125118.Hotel-Information">
-                                <v-img :src="ex" max-width="40" max-height="40" contain class="ml-2"></v-img>
+                                <v-img :src="ex" max-width="30" max-height="30" contain class="ml-2"></v-img>
                             </a>
                         </v-layout>
                     </v-col>
@@ -62,7 +69,7 @@
         </v-footer>
 
         <v-bottom-navigation background-color="black" dark height="50">
-            <p class="text-center">
+            <p class="text-center caption">
                 © Copyright 2020 Wadduwa Beach Villa. All rights reserved
                 <br>
                 Made with ❣ by <a href="https://shivamgoswami.online" target="_blank">Shivam Goswami</a>
@@ -90,7 +97,12 @@
     }
 
     #contactus-list {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
         .contactus-item {
+            width: 350px;
             padding-bottom: 20px;
             display: flex;
             i {
