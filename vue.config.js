2733452564aaf1ec3c0fea602e8444e35d9ce7c1
Shivam Goswami <shiv.goswami@actewagl.com.au>
2020-11-17 12:02:07 +1100

    Removed prerender

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const PrerenderSpaPlugin = require('prerender-spa-plugin');
-
 module.exports = {
   lintOnSave: true,
 
@@ -10,27 +7,5 @@ module.exports = {
         additionalData: '@import "~@/scss/variables.scss";',
       },
     },
-  },
-
-  publicPath: '/',
-  outputDir: path.join(__dirname, 'dist/'),
-  configureWebpack: {
-    plugins: [
-      new PrerenderSpaPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        outputDir: path.join(__dirname, 'dist/'),
-        indexPath: path.join(__dirname, 'dist', '/index.html'),
-        routes: [
-          '/'
-        ],
-        minify: {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          decodeEntities: true,
-          keepClosingSlash: true,
-          sortAttributes: true
-        }
-      }),
-    ],
-  },
+  }
 };
