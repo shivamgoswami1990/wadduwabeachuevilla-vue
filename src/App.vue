35f0bbf298689d6bac096f049faa8754dcfa3311
shiv <shiv15101990@gmail.com>
2022-08-23 00:34:19 +1000

    Added banner for temporary closure

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,86 @@
 <template>
-    <div id="app">
-        <v-app>
-            <nav-drawer :items="navItems" v-if="showDrawer" @showDrawer="transitionend" />
+  <div id="app">
+    <v-app>
+      <nav-drawer :items="navItems" v-if="showDrawer" @showDrawer="transitionend"/>
 
-            <v-app-bar app color="transparent" tile height="70px" elevation="0" hide-on-scroll fixed>
-                <v-app-bar-nav-icon @click="showDrawer = true" aria-label="Menu button">
-                    <slot>
-                        <v-icon size="32" color="primary" aria-label="Menu icon">mdi-text</v-icon>
-                    </slot>
-                </v-app-bar-nav-icon>
-                <a @click="$router.push({ name: 'home'})" class="ml-5">
-                    <v-avatar width="200px" tile>
-                        <v-img :src="logo" contain></v-img>
-                    </v-avatar>
-                </a>
-                <v-spacer></v-spacer>
+      <v-app-bar app color="transparent" tile height="70px" elevation="0" hide-on-scroll fixed>
+        <v-app-bar-nav-icon @click="showDrawer = true" aria-label="Menu button">
+          <slot>
+            <v-icon size="32" color="primary" aria-label="Menu icon">mdi-text</v-icon>
+          </slot>
+        </v-app-bar-nav-icon>
+        <a @click="$router.push({ name: 'home'})" class="ml-5">
+          <v-avatar width="200px" tile>
+            <v-img :src="logo" contain></v-img>
+          </v-avatar>
+        </a>
+        <v-spacer></v-spacer>
 
-                <div class="hidden-md-and-down">
-                    <v-btn text tile height="70" class="font-weight-bold nav-btn" v-for="(item, index) in navItems" :key="index"
-                           v-if="item.isMainNavItem" :to="{ 'name' : item.linkTo}" rel="canonical">
-                        {{item.title}}
-                    </v-btn>
-                </div>
+        <div class="hidden-md-and-down">
+          <v-btn text tile height="70" class="font-weight-bold nav-btn" v-for="(item, index) in navItems" :key="index"
+                 v-if="item.isMainNavItem" :to="{ 'name' : item.linkTo}" rel="canonical">
+            {{ item.title }}
+          </v-btn>
+        </div>
 
-                <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-                <book-now-menu type="button"/>
-            </v-app-bar>
+        <book-now-menu type="button"/>
+      </v-app-bar>
 
-            <v-main>
-                <v-alert color="primary" tile class="ma-0 text-center" light>
-                  Active Management in the COVID-19 Era.
-                    <a class="black--text" style="text-decoration: underline; cursor:pointer; font-weight: bold"
-                       @click="$router.push(
+      <v-main>
+        <v-alert color="primary" tile class="ma-0 text-center" light>
+          Active Management in the COVID-19 Era.
+          <a class="black--text" style="text-decoration: underline; cursor:pointer; font-weight: bold"
+             @click="$router.push(
                             { name: 'post',
                               params: {
                                 name: 'Handling of guest safety in light of COVID-19'.replace(/\s+/g, '-').toLowerCase()
                                 }
                             })">
-                        Please see our response
-                    </a>
-                </v-alert>
-                <router-view/>
-            </v-main>
+            Please see our response
+          </a>
+        </v-alert>
+        <router-view />
 
-            <footer-with-bottom-nav :items="navItems"/>
-        </v-app>
-    </div>
+        <v-dialog v-model="noticeModal" width="400" open-delay="300" origin="center" persistent retain-focus>
+          <v-card width="100%">
+            <v-card-title class="text-h4 font-weight-medium justify-center">Notice</v-card-title>
+            <v-card-text class="font-weight-medium text-h5 text-center">
+              Dear Customers, <br/>
+              Please note that we are temporarily closed for renovations. <br/> <br/>
+
+              Kind regards, <br/>
+              Management
+            </v-card-text>
+            <v-card-actions>
+              <v-btn tile block color="primary" class="elevation-0 black--text font-weight-bold" height="45px" @click="noticeModal = false">
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-main>
+
+      <footer-with-bottom-nav :items="navItems"/>
+    </v-app>
+  </div>
 </template>
 
 <style lang="scss">
-    #app {
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-        a.nav-btn.v-btn--active {
-            color: #FADE03;
-            text-decoration: underline;
-        }
+  a.nav-btn.v-btn--active {
+    color: #FADE03;
+    text-decoration: underline;
+  }
 
-        .pl-large {
-            padding-left: 200px;
-        }
-    }
+  .pl-large {
+    padding-left: 200px;
+  }
+}
 </style>
 
 <script>
@@ -73,22 +91,57 @@ import logo from '@/assets/logo.png';
 
 export default {
   components: {
-    NavDrawer, FooterWithBottomNav, BookNowMenu
+    NavDrawer,
+    FooterWithBottomNav,
+    BookNowMenu
   },
   data() {
     return {
+      noticeModal: true,
       showDrawer: false,
       videoHeight: null,
       logo,
       navItems: [
-        { title: 'Accommodation', isMainNavItem: true, linkTo: 'rooms' },
-        { title: 'Restaurant', isMainNavItem: true, linkTo: 'restaurant' },
-        { title: 'Facilities', isMainNavItem: true, linkTo: 'facilities' },
-        { title: 'Exclusive', isMainNavItem: true, linkTo: 'exclusive' },
-        { title: 'Offers', isMainNavItem: true, linkTo: 'offers' },
-        { title: 'Weddings', isMainNavItem: true, linkTo: 'events' },
-        { title: 'Blog', isMainNavItem: false, linkTo: 'blog' },
-        { title: 'FAQ', isMainNavItem: false, linkTo: 'guidelines' }
+        {
+          title: 'Accommodation',
+          isMainNavItem: true,
+          linkTo: 'rooms'
+        },
+        {
+          title: 'Restaurant',
+          isMainNavItem: true,
+          linkTo: 'restaurant'
+        },
+        {
+          title: 'Facilities',
+          isMainNavItem: true,
+          linkTo: 'facilities'
+        },
+        {
+          title: 'Exclusive',
+          isMainNavItem: true,
+          linkTo: 'exclusive'
+        },
+        {
+          title: 'Offers',
+          isMainNavItem: true,
+          linkTo: 'offers'
+        },
+        {
+          title: 'Weddings',
+          isMainNavItem: true,
+          linkTo: 'events'
+        },
+        {
+          title: 'Blog',
+          isMainNavItem: false,
+          linkTo: 'blog'
+        },
+        {
+          title: 'FAQ',
+          isMainNavItem: false,
+          linkTo: 'guidelines'
+        }
       ]
     };
   },
